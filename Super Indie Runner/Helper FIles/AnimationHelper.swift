c89172dee3e87738e4a76d7418446ce487d70b32
Bryan <bryan@spacegorilla.local>
2018-02-14 06:31:19 +0000

    Update animation helper

---
+++ Super Indie Runner/Helper FIles/AnimationHelper.swift
@@ -0,0 +1,21 @@
+//
+//  AnimationHelper.swift
+//  Super Indie Runner
+//
+//  Created by Bryan on 11/02/2018.
+//  Copyright © 2018 Bryan Mansell. All rights reserved.
+//
+
+import SpriteKit
+
+class AnimationHelper {
+    
+    static func loadTextures(from atlas: SKTextureAtlas, withName name: String) -> [SKTexture] {
+        var textures = [SKTexture]()
+        for index in 0..<atlas.textureNames.count {
+            let textureName = name + String(index)
+            textures.append(atlas.textureNamed(textureName))
+        }
+        return textures
+    }
+}
